e21c9b63dca7710e575ad34eee3f0d7293488cad
0xhatsume <0xhatsume@gmail.com>
2023-08-13 18:18:01 +0800

    :seedling: add initial ui scaffold

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 10,
+    },
+    fs: {
+      strict: true,
+    },
+  },
+  plugins: [react({include: "**/*.tsx",})],
 })
