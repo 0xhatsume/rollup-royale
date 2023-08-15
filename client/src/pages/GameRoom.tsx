b606c56883b4efc32264961ad4052e0c0dec57ed
0xhatsume <0xhatsume@gmail.com>
2023-08-16 00:05:05 +0800

    :sparkles: add initial phaser boiler

--- client/src/pages/GameRoom.tsx
+++ client/src/pages/GameRoom.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react';
+import Phaser from 'phaser';
+import GameSceneFlat from '../phaser/GameSceneFlat';
+import usePhaserGame from '../phaser/usePhaserGame';
 
 const GameRoom = () => {
+  
+  const gameConfig = {
+      type: Phaser.AUTO,
+      parent: "phaser-div",
+      height: 400,
+      scale:{
+          
+          mode: Phaser.Scale.ScaleModes.RESIZE,
+          autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+          
+      },
+      physics:{
+          default: 'arcade',
+          arcade:{ gravity: { y: 0 } }
+      },
+      scene: [GameSceneFlat]
+  }
+  usePhaserGame(gameConfig);
+
   return (
-    <div>GameRoom</div>
+    <div className="w-full md:w-[768px] mx-auto
+            flex flex-col
+            p-0 h-screen
+            border border-green-400
+        "
+      id="phaser-div"
+      >
+          
+      </div>
   )
 }
 
---
+++ client/src/phaser/GameSceneFlat.tsx
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+
+class GameSceneFlat extends Phaser.Scene {
+
+    constructor(){
+        super('GameSceneFlat')
+    }
+
+    preload(){
+        console.log("preload")
+    }
+
+    create(){
+
+    }
+
+
+
+}
+
+export default GameSceneFlat
