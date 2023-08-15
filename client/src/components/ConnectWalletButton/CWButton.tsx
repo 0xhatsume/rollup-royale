8ca2d31769f90ac3ca81ac3440ad51ec31067ecd
0xhatsume <0xhatsume@gmail.com>
2023-08-15 17:22:16 +0800

    :sparkles: add connect wallet

--- client/src/components/ConnectWalletButton/CWButton.tsx
+++ client/src/components/ConnectWalletButton/CWButton.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useAccount, useConnect, useEnsName, useDisconnect } from 'wagmi';
+import { addressShortener } from '../../utils/addressShortener';
 
 const CWButton = () => {
+    const { address, isConnected } = useAccount()
+    const { data: ensName } = useEnsName({ address })
+    const { connect, connectors } = useConnect()
+    const { disconnect } = useDisconnect()
+
+    const shortAddress = addressShortener(address as string)
+    
+    const handleConnect = () => {
+        !isConnected ? connect({ connector: connectors[0] }) : disconnect();
+    }
     return (
         <button type="button" 
             className="text-white bg-prime1
@@ -9,10 +21,10 @@ const CWButton = () => {
             px-4 py-2 text-center mr-3 md:mr-0
             max-w-[132px] md:max-w-none
             "
+
+            onClick={() => handleConnect()}
             >
-            
-            Connect Wallet
-            
+            {isConnected ? (ensName ?? shortAddress) : "Connect Wallet"}
             </button>
     )
 }
