5097c5843c1920b23e131f29b8e0a47aea38c687
0xhatsume <0xhatsume@gmail.com>
2023-08-17 16:42:58 +0800

    :sparkles: add create room modal for link to test call sc

--- client/src/components/LobbyTable/TableBrowserPanel.tsx
+++ client/src/components/LobbyTable/TableBrowserPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RoomSearch from './RoomSearch';
 import TablePagination from './TablePagination';
+import CreateRoomButton from './CreateRoomButton';
 
 const TableBrowserPanel = () => {
     return (
@@ -9,18 +10,7 @@ const TableBrowserPanel = () => {
         grid grid-cols-3
         items-center
         ">
-            <button className="px-4 py-2
-                text-xs sm:text-sm md:text-base
-                text-white font-semibold
-                bg-prime1
-                border-orange-500 rounded-lg
-                h-full w-[8rem] sm:w-auto mx-0 md:mr-7
-                align-middle
-                hover:bg-prime2 hover:text-white
-                focus:ring-palegreen focus:border-palegreen
-                focus:border-2
-            ">
-                Create Game + </button>
+            <CreateRoomButton/>
             <TablePagination/>
             <RoomSearch/>
         </div>
